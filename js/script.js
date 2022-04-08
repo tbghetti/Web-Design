1b1963f429e37f250ade2a0642df10391067c7c5
Thalita Benicio Ghetti <thalita.ghetti@hotmail.com>
2022-04-07 21:14:18 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+if(window.SimpleSlide){
+
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
+
+new SimpleSlide({
+  slide: 'portfolio', 
+  time: 5000,
+  nav: true,
+  pauseOnHover: true, 
+});
+}
+
+if(window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+if(window.SimpleForm) {
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente enviar um email para contato@sunnycakes.com.br</p></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso!</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
+}
+
